987630ba407d9e4c6ca0ca171a4160bd12ab16d2
李子杰 <leechikit0823@163.com>
2017-08-14 19:07:47 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import Disk from './js/createDisk.js';
 import songList from './js/createSongList.js';
 
 
-let disk1 = new Disk({
+new Disk({
 	selector: '#song-wrap-1',
 	soundName: 'Heavy',
 	loop: false
 });
 
-let disk2 = new Disk({
+new Disk({
 	selector: '#song-wrap-2',
 	soundName: 'Good Goodbye',
 	loop: false
